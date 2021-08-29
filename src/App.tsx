46db6d608ccac32876a230fa78c83a0b8c0267b0
Vitalii <varich96@gmail.com>
2021-08-29 21:57:44 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>My Resume</Text>
+    </View>
+  );
+};
+
+export default App;
